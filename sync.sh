7c7c35a8f38083033769ea197cad3b10a3fd1b00
vagrant <vagrant@meanstack-ide-precise64>
2014-02-09 22:34:02 +0100

    indent

--- sync.sh
+++ sync.sh
@@ -2,38 +2,38 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-        if [ ! -d ~/code ]; then
-          mkdir ~/code
-        fi
-        if [ ! -d ~/code/z ]; then
-          git clone https://github.com/rupa/z.git ~/code/z 
-        fi
-        if [ ! -d ~/.vim/bundle/vundle ]; then
-          git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-        fi
-        if [ ! -d ~/.vim/bundle/YouCompleteMe ]; then
-          git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-          sudo apt-get install build-essential cmake python-dev
-          (cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.sh --clang-completer)
-        fi
-        # solarized
-        if [ ! -d ~/.solarized ]; then
-          git clone https://github.com/gmodarelli/solarize.git ~/.solarized
-          chmod +x ~/.solarized/solarize.sh
-        fi
-        ~/.solarized/solarize.sh light
+  if [ ! -d ~/code ]; then
+    mkdir ~/code
+  fi
+  if [ ! -d ~/code/z ]; then
+    git clone https://github.com/rupa/z.git ~/code/z 
+  fi
+  if [ ! -d ~/.vim/bundle/vundle ]; then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  fi
+  if [ ! -d ~/.vim/bundle/YouCompleteMe ]; then
+    git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+    sudo apt-get install build-essential cmake python-dev
+    (cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.sh --clang-completer)
+  fi
+  # solarized
+  if [ ! -d ~/.solarized ]; then
+    git clone https://github.com/gmodarelli/solarize.git ~/.solarized
+    chmod +x ~/.solarized/solarize.sh
+  fi
+  ~/.solarized/solarize.sh light
 
-	rsync --exclude ".git/" --exclude "sync.sh" --exclude "README.md" --exclude "LICENSE" --exclude ".gitkeep" -av . ~
-        vim +BundleInstall +qall
+  rsync --exclude ".git/" --exclude "sync.sh" --exclude "README.md" --exclude "LICENSE" --exclude ".gitkeep" -av . ~
+  vim +BundleInstall +qall
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
 fi
 unset doIt
 source ~/.bash_profile
