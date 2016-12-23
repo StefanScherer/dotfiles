2192ae7133ca772e9ec91c70e1e06b59f55fe8fe
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-23 15:52:44 +0100

    Skip some plugins

--- install-atom-plugins.sh
+++ install-atom-plugins.sh
@@ -5,7 +5,7 @@ apm install cucumber
 apm install editorconfig
 apm install feedback
 apm install file-types
-apm install go-plus
+#apm install go-plus
 apm install language-batch
 apm install language-docker
 apm install language-powershell
@@ -13,7 +13,7 @@ apm install linter
 apm install linter-eslint
 apm install linter-markdown
 apm install linter-shellcheck
-apm install release-notes
+#apm install release-notes
 apm install theme-switcher
 apm install uuidgen
 apm install wordcount
